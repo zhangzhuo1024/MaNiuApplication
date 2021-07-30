e520856f09657081df7e682d8a479c69e7c57850
zz <zhangzhuo1024@163.com>
2021-07-30 22:42:36 +0800

    glide

--- app/src/main/java/com/zz/maniuapplication/MainActivity.java
+++ app/src/main/java/com/zz/maniuapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.zz.maniuapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView viewById = findViewById(R.id.imageView);
+
+        String url = "http://cn.bing.com/az/hprichbg/rb/TOAD_ZH-CN7336795473_1920x1080.jpg";
+
+        Glide.with(this).load(url).into(viewById);
     }
 }
